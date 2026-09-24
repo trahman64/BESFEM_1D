cf69e322a6afc39681dc5d3e0df9b851fbe6733b
huichiayu <hcy@msu.edu>
2026-09-24 09:24:17 -0400

    exit

--- src/linear_diffusion.cpp
+++ src/linear_diffusion.cpp
@@ -79,6 +79,7 @@ void LinearDiffusion::Stepping(mfem::GridFunction source) {
     R_current.AddDomainIntegrator(new mfem::DomainLFIntegrator(reaction));
 
     double f_in = rxn_lf(source);   // now valid -- rxn_lf is a member
+    f_in *= f_in;
     std::cout << f_in << "---" << 2.409e3*0.02e-6*60e-4*t_minus << std::endl;
 //     rxn.Print();
     mfem::ConstantCoefficient nbcCoef(f_in);
